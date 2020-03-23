e77a2d2c7d81d705ae5abefbaecbc1ee3d71e9f6
Cassidy Groenendaal <cassidygroenendaal@gmail.com>
2020-03-22 22:59:45 -0400

    Add function to build display string for months, Add geolocation

--- assets/js/fish.js
+++ assets/js/fish.js
@@ -1,1 +1,94 @@
-const listFish = [];
+const listFish = [
+  {
+    name      : 'Anchovy',
+    months    : {
+      northern  : [
+        { name: 'january', shortName: 'jan', position: 1 },
+        { name: 'february', shortName: 'feb', position: 2 },
+        { name: 'march', shortName: 'mar', position: 3 },
+        { name: 'april', shortName: 'apr', position: 4 },
+        { name: 'may', shortName: 'may', position: 5 },
+        { name: 'june', shortName: 'june', position: 6 },
+        { name: 'july', shortName: 'july', position: 7 },
+        { name: 'august', shortName: 'aug', position: 8 },
+        { name: 'september', shortName: 'sept', position: 9 },
+        { name: 'october', shortName: 'oct', position: 10 },
+        { name: 'november', shortName: 'nov', position: 11 },
+        { name: 'december', shortName: 'dec', position: 12 }
+      ],
+      southern : [
+        { name: 'january', shortName: 'jan', position: 1 },
+        { name: 'february', shortName: 'feb', position: 2 },
+        { name: 'march', shortName: 'mar', position: 3 },
+        { name: 'april', shortName: 'apr', position: 4 },
+        { name: 'may', shortName: 'may', position: 5 },
+        { name: 'june', shortName: 'june', position: 6 },
+        { name: 'july', shortName: 'july', position: 7 },
+        { name: 'august', shortName: 'aug', position: 8 },
+        { name: 'september', shortName: 'sept', position: 9 },
+        { name: 'october', shortName: 'oct', position: 10 },
+        { name: 'november', shortName: 'nov', position: 11 },
+        { name: 'december', shortName: 'dec', position: 12 }
+      ]
+    },
+    location  : {
+      name        : 'River',
+      sublocation : null
+    },
+    startTime : 4,
+    endTime   : 21,
+    sellPrice : 200
+  },
+  {
+    name      : 'Angelfish',
+    months    : {
+      northern  : [
+        { name: 'may', shortName: 'may', position: 5 },
+        { name: 'june', shortName: 'june', position: 6 },
+        { name: 'july', shortName: 'july', position: 7 },
+        { name: 'august', shortName: 'aug', position: 8 },
+        { name: 'september', shortName: 'sept', position: 9 },
+        { name: 'october', shortName: 'oct', position: 10 }
+      ],
+      southern : [
+        { name: 'january', shortName: 'jan', position: 1 },
+        { name: 'february', shortName: 'feb', position: 2 },
+        { name: 'march', shortName: 'mar', position: 3 },
+        { name: 'april', shortName: 'apr', position: 4 },
+        { name: 'november', shortName: 'nov', position: 11 },
+        { name: 'december', shortName: 'dec', position: 12 }
+      ]
+    },
+    location  : {
+      name        : 'River',
+      sublocation : null
+    },
+    startTime : 16,
+    endTime   : 9,
+    sellPrice : 3000
+  },
+  {
+    name      : 'Arowana',
+    months    : {
+      northern  : [
+        { name: 'june', shortName: 'june', position: 6 },
+        { name: 'july', shortName: 'july', position: 7 },
+        { name: 'august', shortName: 'aug', position: 8 },
+        { name: 'september', shortName: 'sept', position: 9 }
+      ],
+      southern : [
+        { name: 'january', shortName: 'jan', position: 1 },
+        { name: 'february', shortName: 'feb', position: 2 },
+        { name: 'march', shortName: 'mar', position: 3 },
+        { name: 'december', shortName: 'dec', position: 12 }
+      ]
+    },
+    location  : {
+      name        : 'River',
+      sublocation : null
+    },
+    startTime : 16,
+    endTime   : 9,
+    sellPrice : 10000
+  }
+];
---
+++ assets/js/script.js
@@ -0,0 +1,73 @@
+//==========================================================================
+// Variables
+//--------------------------------------------------------------------------
+
+let userHemisphere = 'northern';
+
+//==========================================================================
+// Functions
+//--------------------------------------------------------------------------
+
+String.prototype.toTitleCase = function() {
+  return this.replace(/(?:^|\s)\w/g, function(match) {
+    return match.toUpperCase();
+  });
+};
+
+const buildMonthDisplay = months => {
+  if (months.length === 12) {
+    return 'All Year';
+  } else {
+    let displayString = '';
+    let prevMonth = {};
+
+    months.forEach((month, i) => {
+      if (!prevMonth.position) {
+        // Insert first month
+        displayString += `${month.shortName.toTitleCase()} - `;
+      } else if (months[i + 1] === undefined) {
+        // Check if current month is the last month in the list
+        if (month.position !== prevMonth.position + 1) {
+          // Check if the current month is not sequentially after
+          // the previous month, then add the prevMonth to the string
+          displayString += `${prevMonth.shortName.toTitleCase()}, ${month.shortName.toTitleCase()}`;
+        } else {
+          displayString += `${month.shortName.toTitleCase()}`;
+        }
+      } else {
+        // Check to see if we are on the last month
+        if (month.position !== prevMonth.position + 1) {
+          // Check if the current month is not sequentially after
+          // the previous month, then add the prevMonth to the string
+          displayString += `${prevMonth.shortName.toTitleCase()}, ${month.shortName.toTitleCase()} - `;
+        }
+      }
+
+      prevMonth = month;
+    });
+
+    return displayString;
+  }
+};
+
+const init = () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(position => {
+      userHemisphere = position.coords.latitude >= 0 ? 'northern' : 'southern';
+      //run program
+      listFish.forEach(fish => {
+        console.log(
+          fish.name,
+          userHemisphere + ':',
+          buildMonthDisplay(fish.months[userHemisphere])
+        );
+      });
+    });
+  }
+};
+
+//==========================================================================
+// Logic
+//--------------------------------------------------------------------------
+
+init();
